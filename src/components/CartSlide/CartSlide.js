b280db207b14ce46641f52d5a887e2112bd76f14
Rocio <ropiotti@gmail.com>
2020-04-27 10:13:59 +0200

    Se agregó div para agregar función onCloseClick al fondo

--- src/components/CartSlide/CartSlide.js
+++ src/components/CartSlide/CartSlide.js
@@ -14,6 +14,11 @@ class CartSlide extends React.Component {
   render() {
     return (
       <div className="cartSlideBackground">
+        <div
+          className="cartCloseBackground"
+          onClick={() => {
+            this.props.onCloseClick(null)
+          }}></div>
         <div className="cartSlide">
           <div className="cartSlideBox">
             <HeaderSlideCloseLeft onCloseClick={this.props.onCloseClick} />
