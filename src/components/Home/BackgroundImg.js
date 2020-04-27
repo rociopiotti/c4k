ccaefce52e8411ede01bc797e4e3cd7991584e82
Rocio <ropiotti@gmail.com>
2020-04-27 11:06:43 +0200

    Se cambia BackgroundImg a componente funcional

--- src/components/Home/BackgroundImg.js
+++ src/components/Home/BackgroundImg.js
@@ -2,14 +2,14 @@ import React from "react"
 import "./BackgroundImg.scss"
 import backgroundImg from "../../img/backgroundHome.png"
 
-class BackgroundImg extends React.Component {
-  render() {
+const BackgroundImg= (props) =>{
+
     return (
       <div className="backgroundImg">
         <img src={backgroundImg} alt="Background" />
       </div>
     )
-  }
+  
 }
 
 export default BackgroundImg
