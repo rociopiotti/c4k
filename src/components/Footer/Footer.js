b86619188282e70f091c082f20df383eb3d61fa0
Rocio <ropiotti@gmail.com>
2020-04-27 10:47:59 +0200

    Se cambia componente Footer a componente de función

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,16 +4,14 @@ import FooterMenu from "./FooterMenu/FooterMenu"
 import SuscribeBox from "../SuscribeBox/SuscribeBox"
 import SocialLinksMenu from "../SocialLinksMenu/SocialLinksMenu"
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <div className="footer">
-        <FooterMenu />
-        <SuscribeBox />
-        <SocialLinksMenu />
-      </div>
-    )
-  }
+const Footer = (props) => {
+  return (
+    <div className="footer">
+      <FooterMenu />
+      <SuscribeBox />
+      <SocialLinksMenu />
+    </div>
+  )
 }
 
 export default Footer
