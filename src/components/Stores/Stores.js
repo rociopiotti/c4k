a2798d0349b4e32a606af7489624999588ad63f8
Rocio <ropiotti@gmail.com>
2020-04-30 10:18:04 +0200

    Funcionalidad botones que cambian display mode en Stores

--- src/components/Stores/Stores.js
+++ src/components/Stores/Stores.js
@@ -14,32 +14,42 @@ class Stores extends React.Component {
     this.setState({
       currentMode: newState,
     })
+    console.log("Nuevo estado:", newState)
   }
-  setState(displayModeType) {
-    let displayMode
+  setMode(displayModeType) {
+    let modeDisplay
     switch (displayModeType) {
       case "storeAndMap":
-        displayMode = (
+        modeDisplay = (
           <div className="displayModeBox">
             <StoreListItem /> <StoreMap />
           </div>
         )
         break
       case "storeList":
-        displayMode = <StoreListItem />
-
+        modeDisplay = (
+          <div className="displayListBox">
+            <StoreListItem />
+          </div>
+        )
         break
       case "storeMap":
-        displayMode = <StoreMap />
+        modeDisplay = (
+          <div className="displayMapBox">
+            <StoreMap />
+          </div>
+        )
         break
       default:
-        displayMode = null
+        modeDisplay = null
         break
     }
-    return displayMode
+    return modeDisplay
   }
   render() {
-    console.log("------> CURRENT MODE:", this.state.currentMode)
+    console.log("----> CURRENT MODE:", this.state.currentMode)
+    console.log("----> CURRENT DISPLAY:", this.state.displayMode)
+
     return (
       <div className="storePageBox">
         <h2>#STORES</h2>
@@ -47,7 +57,7 @@ class Stores extends React.Component {
           <SearchStore
             onSearchDisplayClick={this.handleSearchStores.bind(this)}
           />
-          {this.setState(this.state.currentMode)}
+          {this.setMode(this.state.currentMode)}
         </div>
       </div>
     )
