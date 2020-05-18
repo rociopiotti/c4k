62e59ead7472793268ad36be0cb9f46c7189a761
Rocio <ropiotti@gmail.com>
2020-05-18 16:48:59 +0200

    Se crea funcion para lista FilterSlide

--- src/components/FilterSlide/FilterSlide.js
+++ src/components/FilterSlide/FilterSlide.js
@@ -9,6 +9,15 @@ import { Timeline, Back } from "gsap/gsap-core";
 
 const ease = Back.easeInOut;
 
+//JSON
+const filsterList = [
+  { id: "0", name: "FILTRO A", label: "FILTRO A" },
+  { id: "1", name: "FILTRO B", label: "FILTRO B" },
+  { id: "2", name: "FILTRO C", label: "FILTRO C" },
+  { id: "3", name: "FILTRO D", label: "FILTRO D" },
+  { id: "4", name: "FILTRO E", label: "FILTRO E" },
+];
+
 class FilterSlide extends React.Component {
   static contextType = PageManagerContext;
 
@@ -42,6 +51,23 @@ class FilterSlide extends React.Component {
     tl.to(this.SlideBackground, { left: 0 });
     tl.to(this.NavCloseBackground, { opacity: 0.8 }, 0.4);
   }
+
+  createList() {
+    return filsterList.map((element, index) => {
+      const { id, name, label } = element;
+
+      return (
+        <li className="listItem" key={id}>
+          <button
+            onClick={() => {
+              this.clickFilterLink(label);
+            }}>
+            {name}
+          </button>
+        </li>
+      );
+    });
+  }
   render() {
     return (
       <div
@@ -57,48 +83,7 @@ class FilterSlide extends React.Component {
           <div className="filterSlideBox">
             <HeaderSlideCloseLeft />
             <h2 className="filterTitle">#FILTER</h2>
-            <ul className="filterSlide">
-              <li>
-                <button
-                  onClick={() => {
-                    this.clickFilterLink("FILTRO A");
-                  }}>
-                  FILTRO A
-                </button>
-              </li>
-              <li>
-                <button
-                  onClick={() => {
-                    this.clickFilterLink("FILTRO B");
-                  }}>
-                  FILTRO B
-                </button>
-              </li>
-              <li>
-                <button
-                  onClick={() => {
-                    this.clickFilterLink("FILTRO C");
-                  }}>
-                  FILTRO C
-                </button>
-              </li>
-              <li>
-                <button
-                  onClick={() => {
-                    this.clickFilterLink("FILTRO D");
-                  }}>
-                  FILTRO D
-                </button>
-              </li>
-              <li>
-                <button
-                  onClick={() => {
-                    this.clickFilterLink("FILTRO E");
-                  }}>
-                  FILTRO E
-                </button>
-              </li>
-            </ul>
+            <ul className="filterSlide">{this.createList()}</ul>
           </div>
         </div>
       </div>
