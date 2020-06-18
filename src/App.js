f9ab4297741040cdbcfb613a03eb5a1d05bf19a0
Rocio <ropiotti@gmail.com>
2020-06-18 15:03:26 +0200

    Ajuste

--- src/App.js
+++ src/App.js
@@ -163,8 +163,8 @@ class App extends React.Component {
   render() {
     if (!this.state.data && !this.state.faqData) {
       return (
-        <div style={{ background: "#000", color: "#fff", textAlign: "center", marginTop: "15vh"}}>
-          ... loading
+        <div style={{ background: "#000", color: "#fff", textAlign: "center" }}>
+          <p style={{ marginTop: "15vh" }}> ... loading</p>
         </div>
       );
     }
