2fe3c020af0186ebf0b3d460e1345e02efeb97e3
Rocio <ropiotti@gmail.com>
2021-09-11 16:52:48 +0200

    Fix white space in utils

--- src/utils/path.js
+++ src/utils/path.js
@@ -1,4 +1,4 @@
 export const URL_PRODUCTS = "https://www.rociopiotti.com/public/webdesign/c4k/db.json";
- export const IMAGES_FOLDER = "https://www.rociopiotti.com/public/webdesign/c4k/images/";
- export const FAQ_DB = "https://www.rociopiotti.com/public/webdesign/c4k/faq.json";
+export const IMAGES_FOLDER = "https://www.rociopiotti.com/public/webdesign/c4k/images/";
+export const FAQ_DB = "https://www.rociopiotti.com/public/webdesign/c4k/faq.json";
 
