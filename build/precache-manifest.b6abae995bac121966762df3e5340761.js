24d3cd60c7e6428d85223c97399e69207c38ed27
Rocio <ropiotti@gmail.com>
2022-03-20 16:54:26 +0100

    Update gitignore and create new deply branch

---
+++ build/precache-manifest.b6abae995bac121966762df3e5340761.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f23e6e194a5c4736e0485074439391fc",
+    "url": "/public/webdesign/c4k/index.html"
+  },
+  {
+    "revision": "18be92ee892e84ff32a8",
+    "url": "/public/webdesign/c4k/static/css/main.2e20c4e6.chunk.css"
+  },
+  {
+    "revision": "38fcc59e238a59900999",
+    "url": "/public/webdesign/c4k/static/js/2.4fd2c2e6.chunk.js"
+  },
+  {
+    "revision": "ddbd4fba0b0f9d8c16e4967c5278e1d2",
+    "url": "/public/webdesign/c4k/static/js/2.4fd2c2e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18be92ee892e84ff32a8",
+    "url": "/public/webdesign/c4k/static/js/main.21f1963c.chunk.js"
+  },
+  {
+    "revision": "b8c8652f9bacc846795b",
+    "url": "/public/webdesign/c4k/static/js/runtime-main.3066c77c.js"
+  },
+  {
+    "revision": "9cc0032937f52f4e99ba8d867aa99d91",
+    "url": "/public/webdesign/c4k/static/media/aboutPhoto.9cc00329.png"
+  },
+  {
+    "revision": "be2e5c24a23d946a95f3272cb8a8d312",
+    "url": "/public/webdesign/c4k/static/media/aboutPhotoLowRes.be2e5c24.png"
+  },
+  {
+    "revision": "5d9d64700027edf074bd8549f3fb0d2f",
+    "url": "/public/webdesign/c4k/static/media/amex.5d9d6470.svg"
+  },
+  {
+    "revision": "95f7f48d34c2102a21fc4abe86651603",
+    "url": "/public/webdesign/c4k/static/media/backgroundHome.95f7f48d.png"
+  },
+  {
+    "revision": "aee57f8a991e6d8985b078b2895d0d07",
+    "url": "/public/webdesign/c4k/static/media/logo.aee57f8a.svg"
+  },
+  {
+    "revision": "c1e1aa13a5ba534981496097a1419263",
+    "url": "/public/webdesign/c4k/static/media/mastercard.c1e1aa13.svg"
+  },
+  {
+    "revision": "65322470fb5382efe1cd8c97cdf00a7e",
+    "url": "/public/webdesign/c4k/static/media/paypal.65322470.svg"
+  },
+  {
+    "revision": "523b7e811a005f40d67dc1131de83bbc",
+    "url": "/public/webdesign/c4k/static/media/visa.523b7e81.svg"
+  }
+]);
